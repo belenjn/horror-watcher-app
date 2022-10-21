c3156990fdb33940dab6fa61325d572c5149d5da
Belen JN <belenjarabanavas31@gmail.com>
2022-10-21 19:20:46 +0200

    WelcomePage created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import { WelcomePage } from './components/welcome-page/WelcomePage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-    App
+    <WelcomePage/>
     </div>
   )
 }
