af1da59d681a67bc84fced4097a271714c28da9b
Belen JN <belenjarabanavas31@gmail.com>
2022-10-21 20:20:01 +0200

    WelcomePage's styles done

--- src/components/welcome-page/WelcomePage.tsx
+++ src/components/welcome-page/WelcomePage.tsx
@@ -4,10 +4,12 @@ export const WelcomePage = () => {
   return (
     <div className="welcomePage__container">
       <div className="webs__container">
-        <div className="web__left"/>
-        <div className="web__right"/>
+        <div className="web__left" />
+        <div className="web__right" />
       </div>
-      WelcomePage
+      <div className="logo" />
+      <h1>Your favorites horror movies at one click </h1>
+      <button>Let's go!</button>
     </div>
-  )
-}
+  );
+};
