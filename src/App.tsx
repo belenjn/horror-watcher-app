38689c94beb663ee8c820ae3108cf1323fd556b3
Belen JN <belenjarabanavas31@gmail.com>
2022-10-21 22:10:55 +0200

    Login's styles done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Login } from './components/login/Login'
 import { WelcomePage } from './components/welcome-page/WelcomePage'
 
 function App() {
 
   return (
     <div className="App">
-    <WelcomePage/>
+    {/* <WelcomePage/> */}
+    <Login/>
     </div>
   )
 }
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,6 +1,28 @@
+import "./Login.css";
 
 export const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login__container">
+      <div className="login__container--data">
+        <div className="logo" />
+
+        <input
+          className="login__container--data--username"
+          type="text"
+          placeholder="Username"
+        />
+        <input
+          className="login__container--data--password"
+          type="password"
+          placeholder="Password"
+        />
+        <div className="login__container--data--buttons">
+            <button className="signIn__button">Sign in</button>
+            <div className="google__button"/>
+        </div>
+
+        <span>It's my first time, I want to <a>sing up</a></span>
+      </div>
+    </div>
+  );
+};
