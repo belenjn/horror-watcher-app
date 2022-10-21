179a408264400b60f43f63cb2ebc14042e4ace3e
Belen JN <belenjarabanavas31@gmail.com>
2022-10-21 19:33:15 +0200

    Webs added in welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
 import { WelcomePage } from './components/welcome-page/WelcomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
--- src/components/welcome-page/WelcomePage.tsx
+++ src/components/welcome-page/WelcomePage.tsx
@@ -1,6 +1,13 @@
+import "./WelcomePage.css";
 
 export const WelcomePage = () => {
   return (
-    <div>WelcomePage</div>
+    <div className="welcomePage__container">
+      <div className="webs__container">
+        <div className="web__left"/>
+        <div className="web__right"/>
+      </div>
+      WelcomePage
+    </div>
   )
 }
