1da6298cb1997b30f86a9276c86a8dbaad2e5e32
Belen JN <belenjarabanavas31@gmail.com>
2022-10-22 13:03:35 +0200

    env.ts created and api call tested

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,40 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { ExplorePage } from './components/explore-page/ExplorePage'
-import { Login } from './components/login/Login'
-import { NotFound } from './components/not-found/NotFound'
-import { Register } from './components/register/Register'
-import { WelcomePage } from './components/welcome-page/WelcomePage'
+import axios from "axios";
+import { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ExplorePage } from "./components/explore-page/ExplorePage";
+import { Login } from "./components/login/Login";
+import { NotFound } from "./components/not-found/NotFound";
+import { Register } from "./components/register/Register";
+import { WelcomePage } from "./components/welcome-page/WelcomePage";
+import { apiKey } from "./env";
 
 function App() {
+  const functionApi = async () => {
+    try {
+      return await axios
+        .get(`https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&with_genres=27`)
+        .then((res) =>console.log(res.data));
+    } catch (error) {
+      console.error(error);
+      throw new Error("Something bad happened");
+    }
+  };
+
+  functionApi();
 
   return (
     <div className="App">
-    <BrowserRouter>
-      <Routes>
-      <Route
-          path="/welcome"
-          element={<WelcomePage/>}
-        />
-        <Route
-          path="/login"
-          element={<Login/>}
-        />
-          <Route
-          path="/register"
-          element={<Register/>}
-        />
-        <Route
-          path="/"
-          element={
-           <ExplorePage/>
-          }
-        />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<ExplorePage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
