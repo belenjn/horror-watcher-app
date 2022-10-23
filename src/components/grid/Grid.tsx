b8c638b5dbf1352aca41373a424d96b99efc7a55
Belen JN <belenjarabanavas31@gmail.com>
2022-10-23 23:36:17 +0200

    Grid's styles done

--- src/components/grid/Grid.tsx
+++ src/components/grid/Grid.tsx
@@ -1,21 +1,32 @@
-import { GridCard } from "../grid-card/GridCard"
-import "./Grid.css"
+import { GridCard } from "../grid-card/GridCard";
+import "./Grid.css";
 
 export const Grid = () => {
   return (
     <div className="grid__container">
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
-        <GridCard/>
+      <div className="grid__container--input">
+      <div className="grid__input--image" />
+        <input
+          className="grid__input"
+          type="text"
+          placeholder="Search by title, year..."
+        />
+      </div>
+
+      <div className="grid__container--items">
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+        <GridCard />
+      </div>
     </div>
-  )
-}
+  );
+};
