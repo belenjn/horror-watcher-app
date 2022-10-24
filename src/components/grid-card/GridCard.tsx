8e7a1a4bd05aa76d7cc2b69433d9ea3ee69bf46f
Belen JN <belenjarabanavas31@gmail.com>
2022-10-24 12:06:33 +0200

    Poster's movie first style done

--- src/components/grid-card/GridCard.tsx
+++ src/components/grid-card/GridCard.tsx
@@ -4,8 +4,18 @@ import "./GridCard.css";
 export const GridCard = ({ movie }: { movie: Movie }) => {
   return (
     <div className="gridCard__container">
-      <div className="gridCard__container--poster" style={{backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.poster_path})`}}>
-        
+      <div
+        className="gridCard__container--poster"
+        style={{
+          backgroundImage: `url(https://image.tmdb.org/t/p/original${movie.poster_path})`,
+        }}
+      >
+        <div className="gridCard__container--poster--icons">
+          <div className="pending" />
+          <div className="favorite" />
+        </div>
+        <div className="more-info">Click here for more info</div>
+
       </div>
     </div>
   );
