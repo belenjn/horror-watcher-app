74f0da327c8b8f9de02e82f1eb49a8ab34142f85
Belen JN <belenjarabanavas31@gmail.com>
2022-10-24 10:59:57 +0200

    Firsts styles for GridCardDetails done

--- src/components/grid-card-details/GridCardDetails.tsx
+++ src/components/grid-card-details/GridCardDetails.tsx
@@ -3,12 +3,13 @@ import "./GridCardDetails.css";
 export const GridCardDetails = () => {
   return (
     <div className="gridCardDetails__container">
+        <div className="gridCardDetails__cross"/>
       <h1 className="gridCardDetails__title">Movie's Title</h1>
       <div className="gridCardDetails__container--movie">
-        <div style={{borderRadius: 10, backgroundColor: "grey", height: 500, width: 300}}/>
-        <h4>
+        <div style={{borderRadius: 10, backgroundColor: "grey", height: 500, width: 300, alignSelf: "center"}}/>
+        <div className="text">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed totam obcaecati illo qui perferendis quidem vel mollitia neque, nobis quaerat hic quo sequi architecto magnam doloremque aspernatur dignissimos. Accusamus, deleniti?
-        </h4>
+        </div>
       </div>
     </div>
   );
