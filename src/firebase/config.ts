8d25a9d1a8543c2dd2ab726fd3d86e8685e38cf8
Belen JN <belenjarabanavas31@gmail.com>
2022-10-26 11:40:48 +0200

    config.ts configured

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { FIREBASE_CONFIG } from "../env";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: FIREBASE_CONFIG.apiKey,
@@ -10,4 +12,8 @@ const firebaseConfig = {
   appId: FIREBASE_CONFIG.appId,
 };
 
-const app = initializeApp(firebaseConfig);
+export const FirebaseApp = initializeApp(firebaseConfig);
+
+export const FirebaseAuth = getAuth(FirebaseApp);
+
+export const FirebaseDB = getFirestore(FirebaseApp);
