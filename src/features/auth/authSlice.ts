4c7b1dbc4fcd4662335bb59d0701343dcd8fc059
Belen JN <belenjarabanavas31@gmail.com>
2022-10-26 10:40:09 +0200

    authSlice configured

---
+++ src/features/auth/authSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export interface StateOfAuth {
+  status: string; //not-authenticated, authenticated
+  userId: null | string;
+  email: null | string;
+  displayName: null | string;
+  errorMessage: null | string;
+}
+
+const initialState: StateOfAuth = {
+  status: "checking", //not-authenticated, authenticated
+  userId: null,
+  email: null,
+  displayName: null,
+  errorMessage: null,
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {},
+    logout: (state, action) => {},
+    checkingCredentials: (state) => {},
+  },
+});
+
+export default authSlice.reducer;
