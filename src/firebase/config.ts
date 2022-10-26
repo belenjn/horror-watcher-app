be98fec9ee99151822823bc2c712eafc73285df2
Belen JN <belenjarabanavas31@gmail.com>
2022-10-26 11:38:36 +0200

    FIREBASE_CONFIG added in env.ts

---
+++ src/firebase/config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { FIREBASE_CONFIG } from "../env";
+
+const firebaseConfig = {
+  apiKey: FIREBASE_CONFIG.apiKey,
+  authDomain: FIREBASE_CONFIG.authDomain,
+  projectId: FIREBASE_CONFIG.projectId,
+  storageBucket: FIREBASE_CONFIG.storageBucket,
+  messagingSenderId: FIREBASE_CONFIG.messagingSenderId,
+  appId: FIREBASE_CONFIG.appId,
+};
+
+const app = initializeApp(firebaseConfig);
