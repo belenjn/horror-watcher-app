f5fa8b157190960265022f1095f7947e5dec21f9
Belen JN <belenjarabanavas31@gmail.com>
2022-11-03 09:09:29 +0100

    useCheckAuth created

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { NotFound } from "./components/not-found/NotFound";
 import { CheckAuth } from "./components/ui/CheckAuth";
 import { WelcomePage } from "./components/welcome-page/WelcomePage";
-import { login, logout } from "./features/auth/authSlice";
-import { FirebaseAuth } from "./firebase/config";
-import { useAppDispatch, useAppSelector } from "./hooks/redux-hooks";
+import { useAppSelector } from "./hooks/redux-hooks";
+import { useCheckAuth } from "./hooks/useCheckAuth";
 import { AuthRoutes } from "./routes/auth/AuthRoutes";
 import { MoviesRoutes } from "./routes/movies/MoviesRoutes";
 import { Movie } from "./types/movie";
@@ -14,21 +11,10 @@ import { Movie } from "./types/movie";
 function App() {
   const moviesLists: Movie[] = useAppSelector((state) => state.movies.movies);
 
-  const { status } = useAppSelector((state) => state.auth);
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    onAuthStateChanged(FirebaseAuth, async (user) => {
-      if (!user) return dispatch(logout({}));
-
-      const { uid, email, displayName } = user;
-
-      dispatch(login({ uid, email, displayName }));
-    });
-  }, []);
+  const { status } = useCheckAuth();
 
   if (status === "checking") {
-    <CheckAuth/>;
+    <CheckAuth />;
   }
 
   return (
---
+++ src/hooks/useCheckAuth.ts
@@ -0,0 +1,24 @@
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { login, logout } from "../features/auth/authSlice";
+import { FirebaseAuth } from "../firebase/config";
+import { useAppDispatch, useAppSelector } from "./redux-hooks";
+
+export const useCheckAuth = () => {
+    const { status } = useAppSelector((state) => state.auth);
+    const dispatch = useAppDispatch();
+  
+    useEffect(() => {
+      onAuthStateChanged(FirebaseAuth, async (user) => {
+        if (!user) return dispatch(logout({}));
+  
+        const { uid, email, displayName } = user;
+  
+        dispatch(login({ uid, email, displayName }));
+      });
+    }, []);
+
+    return {
+        status
+    }
+}
