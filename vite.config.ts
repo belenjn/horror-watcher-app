997353a2df0b8db7f67b43eb5b789e815b9beb43
Belen JN <belenjarabanavas31@gmail.com>
2022-11-09 00:26:46 +0100

    vite.config updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/horror-watcher-app/"
+  base: "/horror-watcher-app/",
+  build: {
+    chunkSizeWarningLimit: 1600,
+  }
 
 })
