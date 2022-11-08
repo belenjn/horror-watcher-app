47b76ded0d2cf86dd2a7eafe8ef4ffc3f3862141
Belen JN <belenjarabanavas31@gmail.com>
2022-11-08 14:42:01 +0100

    Filter's styles done

--- src/components/my-movies-page/filter/Filter.tsx
+++ src/components/my-movies-page/filter/Filter.tsx
@@ -1,12 +1,14 @@
-import {FaFilter} from "react-icons/fa"
-import "./Filter.css"
-
+import { FaFilter } from "react-icons/fa";
+import "./Filter.css";
 
 export const Filter = () => {
   return (
-    <div>
-      <FaFilter/>
-
+    <div className="filter__container">
+      <FaFilter className="filter__icon" />
+      <select className="filter__icon--down" name="Filter">
+        <option value="value1">Older Movies</option>
+        <option value="value2">Newest Movies</option>
+      </select>
     </div>
-  )
-}
+  );
+};
