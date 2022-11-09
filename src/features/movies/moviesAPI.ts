5a88c5d1071f341b5f1f13f1257812f13532abdd
Belen JN <belenjarabanavas31@gmail.com>
2022-11-09 10:17:55 +0100

    trying to get data with fetch

--- src/features/movies/moviesAPI.ts
+++ src/features/movies/moviesAPI.ts
@@ -1,12 +1,17 @@
-import axios from "axios";
 import { apiKey } from "../../env";
 
 export const moviesAPI = async (page: number) => {
   try {
-    return await axios
-      .get(
-        `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&with_genres=27&page=${page}`)
-      .then((res) => res.data.results);
+    const response = await fetch(
+      `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&with_genres=27&page=${page}`,
+      {
+        method: "GET"
+      }
+    );
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse.results;
+    }
   } catch (error) {
     console.error(error);
     throw new Error("Something bad happened");
