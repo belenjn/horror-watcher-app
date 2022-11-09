d29410ff382b186d1052de4c4cc8d5e5d5155cec
Belen JN <belenjarabanavas31@gmail.com>
2022-11-09 10:47:46 +0100

    Solving errors

--- src/features/movies/moviesAPI.ts
+++ src/features/movies/moviesAPI.ts
@@ -5,7 +5,8 @@ export const moviesAPI = async (page: number) => {
   try {
     return await axios
       .get(
-        `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&with_genres=27&page=${page}`)
+        `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&with_genres=27&page=${page}`,
+       )
       .then((res) => res.data.results);
   } catch (error) {
     console.error(error);
